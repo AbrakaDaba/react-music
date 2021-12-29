64cfd41113c25b0704f1e4f9107c2f3f05e51e64
David <abrakadaba@gmail.com>
2021-12-29 08:23:32 +0100

    creating basic components

--- music/src/App.js
+++ music/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { data } from "./data";
+import MusicVideo from "./MusicVideo";
+import Header from "./Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <div className="music">
+        {
+          data.map((el) => {
+            return <MusicVideo key={el.id} el={el} />
+          }
+        )}
+      </div>
+    </>
   );
 }
 
